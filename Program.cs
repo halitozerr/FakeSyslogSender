a28ff3f1befb789594a111f0e42aced552aa76c4
Mustafa Halit Özer <mustafahalitozer@gmail.com>
2021-04-24 19:13:09 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //Port and connection
+            udpClient.Connect("localhost", 517);
+            int a = 1;
+            while (a < 100000)
+            {
+                try
+                {
+                    message = "Is anybody there?";
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+                    udpClient.Send(sendBytes, sendBytes.Length);
+                    Console.WriteLine(a + ". " + "Message sent");
+                    a++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
